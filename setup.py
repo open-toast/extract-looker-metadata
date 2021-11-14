af8fb79bfd21fe073892994d0a11237984acd839
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-11-13 19:21:13 -0500

    add setup

--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
-name='extract-looker-metadata',
-version='1.0.0'
+from setuptools import setup, find_packages
+
+setup(
+    name='extract-looker-metadata',
+    version='0.0.1',
+    description='Extracts adhoc queries from the Looker API to S3',
+    packages=find_packages(include=['looker_ingestion', 'looker_ingestion.*']),
+    install_requires=[
+        'boto3',
+        'looker_sdk'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
