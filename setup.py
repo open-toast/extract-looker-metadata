fb2e2026f9d9008185c1d20117fa7c28ffbf2e23
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-11-19 16:22:22 -0500

    rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='extract-looker-metadata',
+    name='looker_ingestion',
     version='0.0.1',
     description='Extracts adhoc queries from the Looker API to S3',
     packages=find_packages(include=['looker_ingestion', 'looker_ingestion.*']),
