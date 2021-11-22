d70f7b87e1cc5d86526596d49c087a9b8b9e35cb
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-11-21 22:33:07 -0500

    2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='looker_ingestion',
-    version='0.0.1',
+    version='0.0.2',
     description='Extracts adhoc queries from the Looker API to S3',
     packages=find_packages(),
     install_requires=[
