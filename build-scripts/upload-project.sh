a231eba75df626566cd2c5d1a9f6d37243e1569a
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-11-21 20:16:24 -0500

    refactor

---
+++ build-scripts/upload-project.sh
@@ -0,0 +1,12 @@
+# build-scripts/upload-project.sh
+#!/usr/bin/env bash
+
+set -e
+
+PYPI_CONFIG="${HOME}/.pypirc"
+pip install --upgrade pip
+pip install twine
+echo $'[distutils]\nindex-servers = pypi\n[pypi]' > $PYPI_CONFIG
+echo "username=$PYPI_USERNAME" >> $PYPI_CONFIG
+echo "password=$PYPI_PASSWORD" >> $PYPI_CONFIG
+twine upload dist/*.tar.gz
