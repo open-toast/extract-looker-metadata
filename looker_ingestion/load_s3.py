cbe44364706889e7b232d3ba9e615e5f82a552e1
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-12-04 20:50:26 -0500

    line

--- looker_ingestion/load_s3.py
+++ looker_ingestion/load_s3.py
@@ -7,7 +7,6 @@ import boto3
 import json
 import logging
 import csv
-import shlex
 
 def load_object_to_s3(data, local_file_name, output_filename, s3_bucket, 
                     aws_server_public_key=None, aws_server_secret_key=None):
@@ -23,7 +22,7 @@ def load_object_to_s3(data, local_file_name, output_filename, s3_bucket,
         else:
             writer = csv.writer(f, delimiter=',')
             for line in data.split('\n'):
-                writer.writerow(shlex.split(line))
+                writer.writerow(line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");)
 
     if aws_server_public_key is not None:
         session = create_session(aws_server_public_key, aws_server_secret_key)
