ee0b0d306f77ddb89cc871bac271ffbaad552a9a
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-12-04 20:47:56 -0500

    split

--- looker_ingestion/load_s3.py
+++ looker_ingestion/load_s3.py
@@ -23,7 +23,7 @@ def load_object_to_s3(data, local_file_name, output_filename, s3_bucket,
         else:
             writer = csv.writer(f, delimiter=',')
             for line in data.split('\n'):
-                writer.writerow(shlex.splt(line))
+                writer.writerow(shlex.split(line))
 
     if aws_server_public_key is not None:
         session = create_session(aws_server_public_key, aws_server_secret_key)
