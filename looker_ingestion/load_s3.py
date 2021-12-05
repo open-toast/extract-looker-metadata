aef75a1c4dc7ef7963c45b47a3103fcf4114ad44
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-12-05 15:36:02 -0500

    0

--- looker_ingestion/load_s3.py
+++ looker_ingestion/load_s3.py
@@ -60,7 +60,7 @@ def find_existing_data(prefix, s3_bucket, aws_server_public_key=None, aws_server
         file_content = content_object.get()['Body'].read().decode('utf-8')
         if content_object.key.endswith('.json'):
             json_row_objects = [json.loads(line) for line in file_content.splitlines()]
-            print(len(json_row_objects))
+            print(json_row_objects[0])
         elif content_object.key.endswith('.csv'):
             for row in csv.DictReader(file_content.splitlines(True)):
                 json_row_objects.append({k: v for k, v in row.items()})
