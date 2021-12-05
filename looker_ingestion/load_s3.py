7178ee3c70925e7aa3dd47fce82ad446d69b6c57
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-12-05 13:50:44 -0500

    json

--- looker_ingestion/load_s3.py
+++ looker_ingestion/load_s3.py
@@ -58,6 +58,7 @@ def find_existing_data(prefix, s3_bucket, aws_server_public_key=None, aws_server
 
     my_bucket = s3_storage.Bucket(s3_bucket)
     json_objects = []
+    json_row_objects = []
 
     for object_summary in my_bucket.objects.filter(Prefix=prefix):
         content_object = s3_storage.Object(s3_bucket, object_summary.key)
