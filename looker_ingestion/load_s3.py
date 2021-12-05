a777bd43b44024fcbd018ba0c83006001b849325
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-12-05 11:11:34 -0500

    convert to json

--- looker_ingestion/load_s3.py
+++ looker_ingestion/load_s3.py
@@ -58,25 +58,17 @@ def find_existing_data(prefix, s3_bucket, aws_server_public_key=None, aws_server
 
     my_bucket = s3_storage.Bucket(s3_bucket)
     json_objects = []
-    csv_objects = []
-    json_row_objects = []
-    csv_row_objects= []
-    csv_headers = []
 
     for object_summary in my_bucket.objects.filter(Prefix=prefix):
         content_object = s3_storage.Object(s3_bucket, object_summary.key)
         file_content = content_object.get()['Body'].read().decode('utf-8')
-        print(content_object.key)
         if content_object.key.endswith('.json'):
             json_row_objects = [json.loads(line) for line in file_content.splitlines()]
         elif content_object.key.endswith('.csv'):
-            csv_row_objects = csv.reader(file_content.splitlines(True))
-            headers = next(csv_row_objects, None)
-            header_list = list((map(lambda x: x.lower(), headers))) * len(list(csv_row_objects))
+            for row in csv.DictReader(file_content.splitlines(True)):
+                json_row_objects.append({k: int(v) for k, v in row.items()})
         else:
             logging.info("Found file of invalid type, not processing for most recent date")
             break
         json_objects.extend(json_row_objects)
-        csv_objects.extend(csv_row_objects)
-        csv_headers.extend(header_list)
-    return json_objects, csv_objects, csv_headers
+    return json_objects
--- looker_ingestion/sync_data.py
+++ looker_ingestion/sync_data.py
@@ -24,14 +24,14 @@ def extract_query_details(json_filename):
     return queries
 
 
-def find_last_date(query_name, datetime_index, aws_storage_bucket_name, aws_server_public_key, aws_server_secret_key):
+def find_last_date(file_name, datetime_index, aws_storage_bucket_name, aws_server_public_key, aws_server_secret_key):
     """ For the relevant file path, find the date to start extracting data with
     Default: today """
 
     ## if there's no data, get the last day
     first_date = "1 day"
     ## get the largest query time in the data warehouse
-    json_objects, csv_objects, csv_headers = find_existing_data(f"looker/{query_name}/looker_{query_name}", aws_storage_bucket_name, aws_server_public_key, aws_server_secret_key)
+    json_objects, csv_objects, csv_headers = find_existing_data(file_name, aws_storage_bucket_name, aws_server_public_key, aws_server_secret_key)
     last_date = "1990-01-01 00:00:00"
     for last_date_object in json_objects:
         for row in last_date_object:
@@ -94,11 +94,12 @@ def extract_data(json_filename, aws_storage_bucket_name=BUCKET_NAME, aws_server_
         datetime_index = metadata.get("datetime")
         row_limit = query_body.get("limit")
         fields = query_body["fields"]
+        full_file_name = f"looker/{query_name}/{result_format}/{file_name}"
 
         ## if the filter already exists, dont run it
         ## if there's no datetime, don't run it
         if not (datetime_index is None or filters.get(datetime_index) is not None):
-            date_filter = find_last_date(query_name, datetime_index, aws_storage_bucket_name, aws_server_public_key, aws_server_secret_key)
+            date_filter = find_last_date(full_file_name, datetime_index, aws_storage_bucket_name, aws_server_public_key, aws_server_secret_key)
             filters[datetime_index] = f"{date_filter}"
 
         ## hit the Looker API
@@ -122,7 +123,7 @@ def extract_data(json_filename, aws_storage_bucket_name=BUCKET_NAME, aws_server_
                 f"""Hit the limit of {row_limit} rows, try again a smaller window than {date_filter} """
             )
         else:
-            load_object_to_s3(query_run, file_name, f"looker/{query_name}/{file_name}", aws_storage_bucket_name, aws_server_public_key, aws_server_secret_key)
+            load_object_to_s3(query_run, file_name, f"looker/{query_name}/{result_format}/{file_name}", aws_storage_bucket_name, aws_server_public_key, aws_server_secret_key)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process some integers.')
