a5149c2048508958abb913ba88ed7b8c4cfb5a77
alisa-aylward-toast <alisa.aylward@toasttab.com>
2021-12-04 21:46:37 -0500

    print

--- looker_ingestion/load_s3.py
+++ looker_ingestion/load_s3.py
@@ -68,6 +68,7 @@ def find_existing_data(prefix, s3_bucket, aws_server_public_key=None, aws_server
             order_dict_content = csv.DictReader(file_content.splitlines(True))
             ordered_dict_from_csv = list(order_dict_content)[0]
             json_content = dict(ordered_dict_from_csv)
+            print(json_content)
         else:
             logging.info("Found file of invalid type, not processing for most recent date")
             break
