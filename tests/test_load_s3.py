bb036a6a2112f62b2488985c700c601ecf6a75b6
Lis <alisa.aylward@toasttab.com>
2022-04-25 21:17:48 -0400

    change import

--- tests/test_load_s3.py
+++ tests/test_load_s3.py
@@ -4,7 +4,7 @@ from moto import mock_s3
 import sys
 import json 
 import datetime
-import tzutc
+from dateutil.tz import tzutc
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
