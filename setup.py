bf4cfb0b5011392be33f9930f379ca39bc28dbb7
Lis <alisa.aylward@toasttab.com>
2024-05-01 13:38:53 -0400

    pin

--- setup.py
+++ setup.py
@@ -6,15 +6,13 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="looker_ingestion",
-    version="1.4.1",
+    version="1.4.2",
     description="Extracts adhoc queries from the Looker API to S3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/open-toast/extract-looker-metadata",
     packages=find_packages(),
-    entry_points={
-        "console_scripts": ["extract_looker_metadata = looker_ingestion.sync_data:main"]
-    },
+    entry_points={"console_scripts": ["extract_looker_metadata = looker_ingestion.sync_data:main"]},
     install_requires=["boto3", "looker_sdk", "smart_open"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "moto"],
